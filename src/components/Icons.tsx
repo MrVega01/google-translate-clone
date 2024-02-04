06f82ce31f0c1dd018b3472366577960b0da4d3a
MrVega01 <ignaciovega2003@outlook.com>
2024-02-04 01:09:17 -0400

    Add Clipboard and Speaker icons

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -5,3 +5,27 @@ export function ArrowsIcon () {
     </svg>
   )
 }
+export function ClipboardIcon () {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+    >
+      <path fill="none" d="M0 0H24V24H0z"></path>
+      <path d="M16 20H5V6H3v14c0 1.1.9 2 2 2h11v-2zm4-4V4c0-1.1-.9-2-2-2H9c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h9c1.1 0 2-.9 2-2zm-2 0H9V4h9v12z"></path>
+    </svg>
+  )
+}
+export function SpeakerIcon () {
+  return (
+    <svg
+      width="24"
+      height="24"
+      xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
+    >
+      <path d="M3 9v6h4l5 5V4L7 9H3zm13.5 3A4.5 4.5 0 0014 7.97v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z"></path>
+    </svg>
+  )
+}
